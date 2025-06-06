957a3152c08dc4d65f4540b7ca11a951218fff6b
Davis Germain <davisgermain621@gmail.com>
2025-06-06 12:38:47 -0500

    adding main

---
+++ main.py
@@ -0,0 +1,11 @@
+from typing import Final
+from tkinter import *
+
+TEST: Final[str] = "constant"
+#variable name: type annotation[data structure] = ...\
+
+def temp() -> None:
+    print("a")
+
+#returns None by ->
+
