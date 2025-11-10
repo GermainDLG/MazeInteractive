76b97239a44a0f6ce12b379aa1e6e07b152fffa3
Davis Germain <davisgermain621@gmail.com>
2025-11-10 16:15:38 -0500

    Start button and dropdowns
    Start button, algorithm dropdown, and block type drop down working.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pygame_widgets.dropdown import Dropdown
 
 WIDTH = 1200
 HEIGHT = 800
+FOURFIFTHWIDTH = (4*WIDTH)/5
 FPS = 60
 
 
@@ -20,13 +21,50 @@ def set_grid(screen):
         pygame.draw.line(screen, (255,255,255), (colLine*50, 0), (colLine*50, HEIGHT))
     for rowLine in range(HEIGHT//50):
         pygame.draw.line(screen, (255,255,255), (0, rowLine*50), (WIDTH, rowLine*50))
+    pygame.draw.rect(screen, (255,255,255), (950, 0, 250, HEIGHT)) #x,y,width,height
+
 
 
 
 def main():
     screen = pygame_init()
     clock = pygame.time.Clock()
+    gameStart = False
     running = True
+
+    def start_game():
+        nonlocal gameStart
+        gameStart = True
+
+    startButton = Button(screen, 
+                         1000, #excuse the magic numbers
+                         (4*HEIGHT)/5+50, 
+                         150, 50, 
+                         text = "Start", 
+                         inactiveColour = (179,179,179),
+                         pressedColour = (160,160,160), 
+                         onClick = start_game
+                         )
+    
+    blockDropDown = Dropdown(screen,
+                             970,
+                             25, 100,
+                             50,
+                             name = "Block Type",
+                             choices = ["Start", "Goal", "Obstacle"],
+                             values = [1, 2, 3])
+
+    algDropDown = Dropdown(screen,
+                             1080,
+                             25, 100,
+                             50,
+                             name = "Algorithm",
+                             choices = ["BFS", "DFS", "TDB"],
+                             values = [1, 2, 3])
+    
+
+    
+
 
     while running:
         events = pygame.event.get()
@@ -40,9 +78,10 @@ def main():
         screen.fill((0,0,0))
         set_grid(screen)
 
+
         #TO HERE
 
-
+        pygame_widgets.update(events)
         pygame.display.flip()
         clock.tick(FPS)
 
